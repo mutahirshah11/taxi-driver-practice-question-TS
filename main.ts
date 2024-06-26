d6b6d9969dc0f01b29feca254998197c2db3ec2b
mutahirshah11 <mutahirhps@gmail.com>
2024-06-26 12:00:16 +0500

    Taxi fare calculator

---
+++ main.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+
+    const initialKms = 5 ; 
+    const ChargesforinitialKms = 30 ; 
+    const ChargesAfterInitialKms = 25 ; 
+    const FixedCharges = 50 
+
+    
+let CabService = await inquirer.prompt([
+    {
+        name : "CustomerName" , 
+        message : "Enter  Customer Name" ,
+        type : "input"
+    },
+
+    {
+        name : "KmTravelled",
+        message : `Total Km Travlled by customer`,
+        type : "number"  ,
+
+    }
+])
+
+let TotalCharges : unknown
+
+
+if(CabService.KmTravelled >= initialKms){
+     TotalCharges = (CabService.KmTravelled * ChargesforinitialKms) + FixedCharges
+}
+else if (CabService.KmTravelled < initialKms){
+     TotalCharges = (CabService.KmTravelled * ChargesAfterInitialKms)+( (CabService.KmTravelled - initialKms) * ChargesAfterInitialKms ) + FixedCharges 
+}
+else{} 
+
+
+
+
+
+console.log(`Total Kms travelled by Mr.${CabService.CustomerName} is ${CabService.KmTravelled} and his/her Total bill is ${TotalCharges} `);
+
+
